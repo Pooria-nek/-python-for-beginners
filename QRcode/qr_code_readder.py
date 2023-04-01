b2e27f0a12d85fe63eab932748e300718a189519
Mahdi <Mahdi.nekuie@gmail.com>
2023-04-01 21:06:57 +0430

    Update qr_code_readder.py

--- QRcode/qr_code_readder.py
+++ QRcode/qr_code_readder.py
@@ -1,34 +1,22 @@
 import cv2
-from pyzbar.pyzbar import decode
+import pyzbar.pyzbar as pyzbar
 import numpy as np
 
-def decoder(image):
-	# gray image
-	gray_image = cv2.cvtColor(image,0)
-
-	#Decoder
-	barcode = decode(gray_image)
-
-	for obj in barcode:
-		points = obj.polygon
-		(x,y,w,h) = obj.rect
-		pts = np.array(points, np.int32)
-		pts.reshape([-1,1,2])
-		cv2.polylines(image,[pts],True,(0,255,0),3)
-
-		barcodeData = obj.data.decode('utf-8')
-		barcodeType = obj.type
-		
-		# string = "Data : " + barcodeData + " Type : " + barcodeType
-		# cv2.putText(frame,string,(x,y),cv2.FONT_HERSHEY_SIMPLEX,0.5,(255,0,0), 2)
-		print("Barcode: "+barcodeData +" | Type: "+barcodeType)
-
-
 cap = cv2.VideoCapture(0)
+
+
+
 while True:
-	ret,frame = cap.read()
-	decoder(frame)
-	cv2.imshow('image',frame)
-	code = cv2.waitKey(10)
-	if code == ord('q'):
-		break
+    ret, frame = cap.read()
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    decoded = pyzbar.decode(gray)
+    for obj in decoded:
+        print("Data: ", obj.data)
+        pts = np.array([obj.polygon], np.int32,)
+        pts = pts.reshape((-1, 1, 2))
+        cv2.polylines(frame, [pts], True, (0, 0, 255), 2)
+    cv2.imshow("QR", frame)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+cap.release()
+cv2.destroyAllWindows()
