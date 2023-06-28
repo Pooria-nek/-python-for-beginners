d07d138179b3ae498ba434ec1c1b4bffeac12cf8
Mahdi <Mahdi.nekuie@gmail.com>
2023-06-28 19:18:11 +0330

    Update qr_code_tkinterUI.py

--- QRcode/qr_code_tkinterUI.py
+++ QRcode/qr_code_tkinterUI.py
@@ -1,24 +1,33 @@
 import pyqrcode
 import tkinter as tk
+from tkinter import *
 from PIL import ImageTk, Image
 
 def generate_qr():
     qr_data = qr_text.get()
     qr = pyqrcode.create(qr_data)
     qr.png('qr.png', scale=8)
-    img = ImageTk.PhotoImage(Image.open('qr.png'))
-    panel = tk.Label(root, image=img)
+
+    img = Image.open('qr.png')
+    img = img.resize((300, 300), Image.ANTIALIAS)
+    img = ImageTk.PhotoImage(img)
+    panel.configure(image=img)
     panel.image = img
-    panel.pack()
 
 root = tk.Tk()
 root.title("QR Code Generator")
-root.geometry("300x300")
+root.geometry("400x400")
 
-qr_text = tk.Entry(root, width=30)
-qr_text.pack(pady=10)
+frame = Frame(root)
+frame.pack()
 
-generate_button = tk.Button(root, text="Generate", command=generate_qr)
-generate_button.pack(pady=10)
+generate_button = tk.Button(frame, text="Generate QR Code", command=generate_qr)
+generate_button.pack(side=LEFT, pady=10, padx=10)
+
+qr_text = tk.Entry(frame, font=("Helvetica", 16))
+qr_text.pack(side=LEFT, pady=10, padx=10)
+
+panel = tk.Label(root)
+panel.pack(pady=10)
 
 root.mainloop()
