3c302cd82cbe2d057f946d69ee7f93bf60a1096d
Mahdi <Mahdi.nekuie@gmail.com>
2023-07-01 12:29:01 +0330

    Update qr_code_readder_tkinterUI.py

--- QRcode/qr_code_readder_tkinterUI.py
+++ QRcode/qr_code_readder_tkinterUI.py
@@ -1,22 +1,48 @@
 import cv2
-import pyzbar.pyzbar as pyzbar
-import numpy as np
+from pyzbar.pyzbar import decode
+import tkinter as tk
 
-cap = cv2.VideoCapture(0)
+class QRCodeScanner:
+    def __init__(self):
+        # Create the main window
+        self.root = tk.Tk()
+        self.root.title('QR Code Scanner')
 
+        # Create the label to display the result
+        self.result_label = tk.Label(self.root, text='Scan a QR code')
+        self.result_label.pack()
 
+        # Create the button to start scanning
+        self.scan_button = tk.Button(self.root, text='Scan', command=self.scan)
+        self.scan_button.pack()
 
-while True:
-    ret, frame = cap.read()
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    decoded = pyzbar.decode(gray)
-    for obj in decoded:
-        print("Data: ", obj.data)
-        pts = np.array([obj.polygon], np.int32,)
-        pts = pts.reshape((-1, 1, 2))
-        cv2.polylines(frame, [pts], True, (0, 0, 255), 2)
-    cv2.imshow("QR", frame)
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
-cap.release()
-cv2.destroyAllWindows()
+    def scan(self):
+        # Open the camera
+        cap = cv2.VideoCapture(0)
+
+        while True:
+            # Read a frame from the camera
+            ret, frame = cap.read()
+
+            # Decode the QR code if it is present in the frame
+            decoded_objects = decode(frame)
+            for obj in decoded_objects:
+                self.result_label.config(text=obj.data.decode())
+
+            # Display the frame
+            cv2.imshow('frame', frame)
+
+            # Exit if 'q' is pressed
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                break
+
+        # Release the camera and close all windows
+        cap.release()
+        cv2.destroyAllWindows()
+
+    def run(self):
+        self.root.mainloop()
+
+if __name__ == '__main__':
+    scanner = QRCodeScanner()
+    scanner.run()
