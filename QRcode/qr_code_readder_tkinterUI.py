cdb9de915d19df6fbf8a59994fe1bf2c62046826
Mahdi <Mahdi.nekuie@gmail.com>
2023-07-01 12:24:14 +0330

    Create qr_code_readder_tkinterUI.py

---
+++ QRcode/qr_code_readder_tkinterUI.py
@@ -0,0 +1,22 @@
+import cv2
+import pyzbar.pyzbar as pyzbar
+import numpy as np
+
+cap = cv2.VideoCapture(0)
+
+
+
+while True:
+    ret, frame = cap.read()
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    decoded = pyzbar.decode(gray)
+    for obj in decoded:
+        print("Data: ", obj.data)
+        pts = np.array([obj.polygon], np.int32,)
+        pts = pts.reshape((-1, 1, 2))
+        cv2.polylines(frame, [pts], True, (0, 0, 255), 2)
+    cv2.imshow("QR", frame)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+cap.release()
+cv2.destroyAllWindows()
