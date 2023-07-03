d4342aa85848645a022efd90586bb7997a5ea6c8
Mahdi <Mahdi.nekuie@gmail.com>
2023-07-04 00:35:42 +0330

    Update face_detection.py

--- opencv/face_detection.py
+++ opencv/face_detection.py
@@ -1,12 +1,30 @@
+import mediapipe as mp
+import numpy as np
 import cv2
 
 cap = cv2.VideoCapture(0)
 
-while True:
-    rer, frame = cap.read()
+facemesh = mp.solutions.face_mesh
+face = facemesh.FaceMesh()
+draw = mp.solutions.drawing_utils
 
-    cv2.imshow('image', frame)
-    code = cv2.waitKey(10)
-    
-    if code == ord('q'):
+while True:
+    ret,frame = cap.read()
+
+    rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+
+    op = face.process(rgb)
+    # print(dir(op))
+    print(op.multi_face_landmarks)
+
+    if op.multi_face_landmarks:
+        for i in op.multi_face_landmarks:
+            draw.draw_landmarks(frame, i)
+
+
+    cv2.imshow("face detect", frame)
+
+    if cv2.waitKey(1) == 27:
+        cap.release()
+        cv2.destroyAllWindows()
         break
