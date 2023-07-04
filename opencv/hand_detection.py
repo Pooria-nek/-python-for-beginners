681422e601f031b2205725b934a152eda8ebc18c
Mahdi <Mahdi.nekuie@gmail.com>
2023-07-04 11:28:30 +0330

    Update hand_detection.py

--- opencv/hand_detection.py
+++ opencv/hand_detection.py
@@ -1,43 +1,43 @@
 import cv2
 import mediapipe as mp
-mp_drawing = mp.solutions.drawing_utils
-mp_drawing_styles = mp.solutions.drawing_styles
-mp_hands = mp.solutions.hands
-
+import time
 cap = cv2.VideoCapture(0)
 
-with mp_hands.Hands(
-    model_complexity=0,
-    min_detection_confidence=0.5,
-    min_tracking_confidence=0.5) as hands:
-  while cap.isOpened():
-    success, image = cap.read()
-    if not success:
-      print("Ignoring empty camera frame.")
-      # If loading a video, use 'break' instead of 'continue'.
-      continue
+mp_handmesh = mp.solutions.hands
+mp_hands = mp_handmesh.Hands(static_image_mode=False,
+                            max_num_hands=2,
+                            model_complexity=1,
+                            min_detection_confidence=0.5,
+                            min_tracking_confidence=0.5)
 
-    # To improve performance, optionally mark the image as not writeable to
-    # pass by reference.
-    image.flags.writeable = False
-    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-    results = hands.process(image)
+mp_drawing = mp.solutions.drawing_utils
 
-    # Draw the hand annotations on the image.
-    image.flags.writeable = True
-    image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+pTime = 0
+cTime = 0
+
+while True:
+    success, frame = cap.read()
+    imgRGB = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+    results = mp_hands.process(imgRGB)
+    #print(results.multi_hand_landmarks)
+
     if results.multi_hand_landmarks:
-      for hand_landmarks in results.multi_hand_landmarks:
-        mp_drawing.draw_landmarks(
-            image,
-            hand_landmarks,
-            mp_hands.HAND_CONNECTIONS,
-            mp_drawing_styles.get_default_hand_landmarks_style(),
-            mp_drawing_styles.get_default_hand_connections_style())
-        
-    # Flip the image horizontally for a selfie-view display.
-    cv2.imshow('MediaPipe Hands', cv2.flip(image, 1))
-    if cv2.waitKey(5) & 0xFF == 27:
-      break
+        for handLms in results.multi_hand_landmarks:
+            for id, lm in enumerate(handLms.landmark):
+                #print(id,lm)
+                h, w, c = frame.shape
+                cx, cy = int(lm.x *w), int(lm.y*h)
+                #if id ==0:
+                cv2.circle(frame, (cx,cy), 3, (255,0,255), cv2.FILLED)
 
-cap.release()
+            mp_drawing.draw_landmarks(frame, handLms, mp_handmesh.HAND_CONNECTIONS)
+
+
+    cTime = time.time()
+    fps = 1/(cTime-pTime)
+    pTime = cTime
+
+    cv2.putText(frame,str(int(fps)), (10,70), cv2.FONT_HERSHEY_PLAIN, 3, (255,0,255), 3)
+
+    cv2.imshow("Image", frame)
+    cv2.waitKey(1)
